9590a8f2b99989e7deb9b00c125c23f5cce17bef
Fabio Capucci <fabio.cappuc@gmail.com>
2026-03-18 11:43:30 +0100

    removed timezone setting

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,20 +18,4 @@ if [[ ! -z "$COMPOSER_GITLAB" ]]; then
   composer config --global gitlab-token.gitlab.com "$COMPOSER_GITLAB"
 fi
 
-#
-# If $TIMEZONE variable is passed to the image - it will set system timezone 
-# and php.ini date.timezone value as well
-# Overwise the default system Etc/UTC timezone will be used
-#
-# Also you can set the php timezone with direct setting it in php.ini 
-# within your .gitlab-ci.yml like
-# before_script:
-# - echo "America/New_York" > /usr/local/etc/php/conf.d/timezone.ini
-
-if [[ ! -z "$TIMEZONE" ]]; then
-  sudo echo "$TIMEZONE" > /etc/timezone
-  sudo dpkg-reconfigure -f noninteractive tzdata
-fi
-echo "date.timezone=$(cat /etc/timezone)" | sudo tee ${PHP_INI_DIR}/conf.d/timezone.ini > /dev/null
-
 exec "$@"
